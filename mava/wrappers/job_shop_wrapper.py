e11d157bb0d603c898c7b39e78a18b60e29a18fa
AminAvan <amin.avan@ontariotechu.net>
2025-06-18 15:46:32 -0400

    In job_shop_wrapper.py, I have copied 'def aggregate_rewards' and 'class JumanjiMarlWrapper' as I think it is common between mava/wrappers/jumanji.py and mava/wrappers/job_shop_wrapper.py .

--- mava/wrappers/job_shop_wrapper.py
+++ mava/wrappers/job_shop_wrapper.py
@@ -1,3 +1,8 @@
+# a draft by Amin Avan for writting a wrapper for JobShop in jumanji
+# that the source is located in
+# https://github.com/instadeepai/jumanji/tree/main/jumanji/environments/packing/job_shop
+
+
 from abc import ABC, abstractmethod
 from functools import cached_property
 from typing import Any, Dict, Tuple, Union
@@ -10,5 +15,105 @@ from jumanji.env import Environment
 
 from jumanji.environments.packing.job_shop import JobShop
 
+from jumanji.types import TimeStep
+from jumanji.wrappers import Wrapper
+
+from mava.types import Observation, ObservationGlobalState, State
+
 env = JobShop()
 print(type(env))
+
+def aggregate_rewards(reward: chex.Array, num_agents: int) -> chex.Array:
+    """Aggregate individual rewards across agents."""
+    """copied from mava/wrappers/jumanji.py"""
+    team_reward = jnp.sum(reward)
+    return jnp.repeat(team_reward, num_agents)
+
+
+class JumanjiMarlWrapper(Wrapper, ABC):
+    """copied from mava/wrappers/jumanji.py"""
+    def __init__(self, env: Environment, add_global_state: bool):
+        self.add_global_state = add_global_state
+        super().__init__(env)
+        self.num_agents = self._env.num_agents
+        self.time_limit = self._env.time_limit
+
+    @abstractmethod
+    def modify_timestep(self, timestep: TimeStep) -> TimeStep[Observation]:
+        """Modify the timestep for `step` and `reset`."""
+        pass
+
+    def get_global_state(self, obs: Observation) -> chex.Array:
+        """The default way to create a global state for an environment if it has no
+        available global state - concatenate all observations.
+        """
+        global_state = jnp.concatenate(obs.agents_view, axis=0)
+        global_state = jnp.tile(global_state, (self._env.num_agents, 1))
+        return global_state
+
+    def reset(self, key: chex.PRNGKey) -> Tuple[State, TimeStep]:
+        """Reset the environment."""
+        state, timestep = self._env.reset(key)
+        timestep = self.modify_timestep(timestep)
+        if self.add_global_state:
+            global_state = self.get_global_state(timestep.observation)
+            observation = ObservationGlobalState(
+                global_state=global_state,
+                agents_view=timestep.observation.agents_view,
+                action_mask=timestep.observation.action_mask,
+                step_count=timestep.observation.step_count,
+            )
+            return state, timestep.replace(observation=observation)
+
+        return state, timestep
+
+    def step(self, state: State, action: chex.Array) -> Tuple[State, TimeStep]:
+        """Step the environment."""
+        state, timestep = self._env.step(state, action)
+        timestep = self.modify_timestep(timestep)
+        if self.add_global_state:
+            global_state = self.get_global_state(timestep.observation)
+            observation = ObservationGlobalState(
+                global_state=global_state,
+                agents_view=timestep.observation.agents_view,
+                action_mask=timestep.observation.action_mask,
+                step_count=timestep.observation.step_count,
+            )
+            return state, timestep.replace(observation=observation)
+
+        return state, timestep
+
+    @cached_property
+    def observation_spec(self) -> specs.Spec[Union[Observation, ObservationGlobalState]]:
+        """Specification of the observation of the environment."""
+        step_count = specs.BoundedArray(
+            (self.num_agents,),
+            int,
+            jnp.zeros(self.num_agents, dtype=int),
+            jnp.repeat(self.time_limit, self.num_agents),
+            "step_count",
+        )
+
+        obs_spec = self._env.observation_spec
+        obs_data = {
+            "agents_view": obs_spec.agents_view,
+            "action_mask": obs_spec.action_mask,
+            "step_count": step_count,
+        }
+
+        if self.add_global_state:
+            num_obs_features = obs_spec.agents_view.shape[-1]
+            global_state = specs.Array(
+                (self._env.num_agents, self._env.num_agents * num_obs_features),
+                obs_spec.agents_view.dtype,
+                "global_state",
+            )
+            obs_data["global_state"] = global_state
+            return specs.Spec(ObservationGlobalState, "ObservationSpec", **obs_data)
+
+        return specs.Spec(Observation, "ObservationSpec", **obs_data)
+
+    @cached_property
+    def action_dim(self) -> chex.Array:
+        """Get the actions dim for each agent."""
+        return int(self._env.action_spec.num_values[0])
