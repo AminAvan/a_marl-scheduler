e2f12e8fa51af3effa5ea89535d39f2890243f30
AminAvan <amin.avan@ontariotechu.net>
2025-06-26 15:41:49 -0400

    trying to add 'network' of JobShop into Mava.

--- mava/networks/job_shop_network.py
+++ mava/networks/job_shop_network.py
@@ -1,34 +1,12 @@
-### add amin
 import jax
-import haiku as hk
 from mava.networks.base import FeedForwardActor, FeedForwardValueNet
-from jumanji.training.networks.job_shop.actor_critic import (
-    make_actor_network_job_shop,
-    make_critic_network_job_shop,
-)
+from jumanji.training.networks.job_shop.actor_critic import JobShopTorso
 
-class JobShopNetwork(Network):
-    """Wraps Jumanji’s JobShop GNN as a Mava Network."""
+class JobShopActor(FeedForwardActor):
+    """Actor using Jumanji's JobShop GNN torso."""
+    torso: JobShopTorso
 
-    def __init__(self, signature, key: jax.random.PRNGKey, config):
-        super().__init__(signature, key)
-
-        # Build the actor + critic Haiku fns using Jumanji’s factories
-        actor_init, actor_apply = make_actor_network_job_shop(
-            **config.job_shop.actor  # your hyperparams
-        )
-        critic_init, critic_apply = make_critic_network_job_shop(
-            **config.job_shop.critic  # your hyperparams
-        )
-
-        # Store for Mava’s base.train and base.eval calls
-        self.actor_init = actor_init
-        self.actor_apply = actor_apply
-        self.critic_init = critic_init
-        self.critic_apply = critic_apply
-
-    def __call__(self, params, rng, observation):
-        # returns (logits, values) per Mava’s API
-        logits = self.actor_apply(params.actor, rng, observation)
-        values = self.critic_apply(params.critic, rng, observation)
-        return logits, values
+class JobShopCritic(FeedForwardValueNet):
+    """Critic using Jumanji's JobShop GNN torso."""
+    torso: JobShopTorso
+    centralised_critic: bool = False
